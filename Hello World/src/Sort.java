10b0f04d6cac567d920e6af2254bc2deac6b8098
1861375 <1861375@01-19832.pusd.dom>
2021-11-05 09:31:12 -0700

    Complete ArrayList sorting methods

--- Hello World/src/Sort.java
+++ Hello World/src/Sort.java
@@ -1,78 +1,116 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Sort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] values = new int[1000];
+		ArrayList<Integer> nums = new ArrayList<Integer>();
 
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		ArrayList<Integer> bubbleArr = new ArrayList<Integer>();
-		ArrayList<Integer> selArr = new ArrayList<Integer>();
-		ArrayList<Integer> insArr = new ArrayList<Integer>();
-		int max = 30;
-		int min = 1;
-
-		for (int i = 0; i < 20; i++) {
-			int temp = (int) (Math.random() * (max - min + 1)) + min;
-			arr.add(temp);
-			bubbleArr.add(temp);
-			selArr.add(temp);
-			insArr.add(temp);
+		for (int i = 0; i < values.length; i++) {
+			values[i] = (int) (Math.random() * 100 + 1);
+			nums.add(values[i]);
 		}
 
-		System.out.println("\nGenerated random, unsorted list:");
-		System.out.println(arr);
+		printArr(nums);
+		insertionSort(nums);
+		printArr(nums);
 
-		System.out.println("\n\nBubble sort:");
-		System.out.println(bubble(bubbleArr));
-
-		System.out.println("\n\nSelection sort");
-		System.out.println(selection(selArr));
+		// for 1000 values, selection sort always goes 499500 times
+		// insertion sort is around 250000
 
 	}
 
-	public static ArrayList<Integer> selection(ArrayList<Integer> nums) {
-		for (int i = 0; i < nums.size(); i++) {
-			/*
-			
-			
-			*/
+	public static void insertionSort(ArrayList<Integer> elem) {
+		int counter = 0;
+		// traverses the array, starting at index 0
+		for (int j = 1; j < elem.size(); j++) {
+			// [6,5,9,1,3]
+			int temp = elem.remove(j);
+			// [6,9,1,3]
+			int possibleIndex = j;
+			while (possibleIndex > 0 && temp < elem.get(possibleIndex - 1)) {
+				possibleIndex--; // counts down from j
+				counter++;
+			}
+			elem.add(possibleIndex, temp);
 		}
-
-		return nums;
+		System.out.println(counter);
 	}
 
-//	public static ArrayList<Integer> selection(ArrayList<Integer> nums) {
-//		for (int i = 0; i < nums.size(); i++) {
-//			int temp = nums.get(i);
-//			nums.remove(i);
-//			
-//			for (int j = 0; j < i; j++) {
-//				int check = nums.get(j);
-//				if (temp < check) {
-//					continue;
-//				}
-//			}
-//		}
-//		
-//		return nums;
-//	}
-
-	public static ArrayList<Integer> bubble(ArrayList<Integer> nums) {
-		for (int j = 0; j < nums.size(); j++) {
-			for (int i = 0; i < nums.size() - 1; i++) {
-				int num1 = nums.get(i);
-				int num2 = nums.get(i + 1);
-
-				// number @ i is greater than number @ i + 1
-				if (num1 > num2) {
-					nums.set(i, num2);
-					nums.set(i + 1, num1);
+	public static void selectionSort(ArrayList<Integer> elem) {
+		int counter = 0;
+		for (int j = 0; j < elem.size() - 1; j++) {
+			int minIndex = j;
+			for (int k = j + 1; k < elem.size(); k++) {
+				if (elem.get(k) < elem.get(minIndex)) { // looks for smallest
+					minIndex = k;
 				}
+				counter++;
+			}
+			int temp = elem.get(j);
+			elem.set(j, elem.get(minIndex));
+			elem.set(minIndex, temp);
+		}
+		System.out.println(counter);
+	}
+
+	public static void printArr(ArrayList<Integer> elem) {
+		System.out.println("\n\n");
+		int i = 0;
+		for (int j : elem) {
+			System.out.print(j);
+			i++;
+
+			if (i < elem.size()) {
+				System.out.print(",");
 			}
 		}
-
-		return nums;
+		System.out.println("\n\n");
 	}
 
+	public static void printArr(int[] elem) {
+		System.out.println("\n\n");
+		int i = 0;
+		for (int j : elem) {
+			System.out.print(j);
+			i++;
+			if (i < elem.length) {
+				System.out.print(",");
+			}
+		}
+		System.out.println("\n\n");
+	}
+
+	public static void insertionSort(int[] elem) {
+		int counter = 0;
+		// traverses the array, starting at index 0
+		for (int j = 1; j < elem.length; j++) {
+			int temp = elem[j];
+			int possibleIndex = j;
+			while (possibleIndex > 0 && temp < elem[possibleIndex - 1]) {
+				elem[possibleIndex] = elem[possibleIndex - 1]; // shifts elements to the right
+				possibleIndex--; // counts down from j
+				counter++;
+			}
+			elem[possibleIndex] = temp;
+		}
+		System.out.println(counter);
+	}
+
+	public static void selectionSort(int[] elem) {
+		int counter = 0;
+		for (int j = 0; j < elem.length - 1; j++) {
+			int minIndex = j;
+			for (int k = j + 1; k < elem.length; k++) {
+				if (elem[k] < elem[minIndex]) { // looks for smallest
+					minIndex = k;
+				}
+				counter++;
+			}
+			int temp = elem[j];
+			elem[j] = elem[minIndex];
+			elem[minIndex] = temp;
+		}
+		System.out.println(counter);
+	}
 }
