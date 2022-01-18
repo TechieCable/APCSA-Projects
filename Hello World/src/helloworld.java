bf1cc38078c940c9d02666103405754378333e1f
1861375 <1861375@01-19832.pusd.dom>
2022-01-18 09:27:29 -0800

    Classnotes 1/18/2022

--- Hello World/src/helloworld.java
+++ Hello World/src/helloworld.java
@@ -3,10 +3,14 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("hello world!");
-		
+
 		System.out.println("print to the console.");
+		for (int i = 0; i < 1; i++) {
+			System.out.println(i);
+		}
+
 	}
 
 }
