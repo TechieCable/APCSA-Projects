06333f3b405d9ae2d14136e2540dab806b347992
1861375 <1861375@01-19643.pusd.dom>
2022-03-01 09:34:12 -0800

    Classwork 3/1/2022

--- Recursive Drawing/src/Recursion.java
+++ Recursive Drawing/src/Recursion.java
@@ -9,7 +9,29 @@ import javax.swing.Timer;
 
 public class Recursion extends JPanel implements ActionListener {
 
-	int CCdelta = 10;
+	static int screenL = 1000;
+	static final double pi = Math.PI;
+	int branchHue = 0;
+
+	/**
+	 * converts HSV to Color object
+	 * 
+	 * - hue should be 0-360
+	 * 
+	 * - saturation should be 0-100
+	 * 
+	 * - value should be 0-100
+	 * 
+	 * @param h
+	 * @param s
+	 * @param v
+	 * @return
+	 */
+	public Color hsv2C(int h, int s, int v) {
+		int rgb = Color.HSBtoRGB((float) (h / 360.0), (float) (s / 100.0), (float) (v / 100.0));
+		int r = (rgb >> 16) & 0xFF, g = (rgb >> 8) & 0xFF, b = rgb & 0xFF;
+		return new Color(r, g, b);
+	}
 
 	/**
 	 * draws concentric circles
@@ -19,18 +41,15 @@ public class Recursion extends JPanel implements ActionListener {
 	 * @param x
 	 * @param y
 	 */
-	public void concentricCircles(Graphics g, int width, int x, int y) {
+	public void concentricCircles(Graphics g, int width, int x, int y, int delta) {
 		if (width < 1) {
 			return;
 		}
 
-		// each method call draws a circle
-		// then call itself to draw a different circle
 		g.setColor(Color.BLUE);
 		g.drawOval(x, y, width, width);
 
-		concentricCircles(g, width - CCdelta, x + CCdelta / 2, y + CCdelta / 2);
-
+		concentricCircles(g, width - delta, x + delta / 2, y + delta / 2, delta);
 	}
 
 	/**
@@ -42,7 +61,7 @@ public class Recursion extends JPanel implements ActionListener {
 	 * @param width
 	 */
 	public void squares(Graphics g, int x, int y, int width) {
-		if (width < 10) {
+		if (width < 1) {
 			return;
 		}
 		int seg = width / 3;
@@ -75,34 +94,89 @@ public class Recursion extends JPanel implements ActionListener {
 
 	}
 
+	/**
+	 * draw clover figure
+	 * 
+	 * @param g
+	 * @param x
+	 * @param y
+	 * @param width
+	 */
 	public void clover(Graphics g, int x, int y, int width) {
 		if (width < 1) {
 			return;
 		}
-		g.setColor(Color.WHITE);
+		g.setColor(Color.GREEN);
 
 		g.drawOval(x - width, y - width, width, width);
 		g.drawOval(x, y - width, width, width);
 		g.drawOval(x, y, width, width);
-//		g.drawOval(width - x, width - y, width, width);
+		g.drawOval(x - width, y, width, width);
 
-		clover(g, x, y, width - 10);
+		clover(g, x, y, width - 5);
+
+	}
+
+	public void flake(Graphics g, int x, int y, double length, int hue) {
+		if (length < 1) {
+			return;
+		}
+		if (hue > 360) {
+			hue = 0;
+		}
+
+		for (int i = 1; i < 7; i++) {
+			int x2 = x + (int) (length * Math.cos(pi * i / 3));
+			int y2 = y + (int) (length * Math.sin(pi * i / 3));
+
+			g.setColor(hsv2C(hue, 100, 100));
+			g.drawLine(x, y, x2, y2);
+
+			flake(g, x2, y2, length / 2.6, hue + 30);
+		}
+	}
+
+	public void treeBranch(Graphics g, int x, int y, int length, double angle, int hue) {
+		if (length < 1) {
+			return;
+		}
+
+		// 3pi/12, 5pi/12, 7pi/12, 9pi/12
+		// -3pi/12, -1pi/12, 1pi/12, 3pi/12
+
+		// i goes from -3 to 3
+		for (int i = -3; i < 5; i += 2) {
+			int x2 = x + (int) (length * Math.cos(angle + i * pi / 12));
+			int y2 = y - (int) (length * Math.sin(angle + i * pi / 12));
+			g.setColor(hsv2C(hue, 100, 100));
+			g.drawLine(x, y, x2, y2);
+			treeBranch(g, x2, y2, length / 2, angle + i * pi / 12, hue + 30);
+		}
 
 	}
 
 	public void paint(Graphics g) {
+		screenL = this.getHeight();
+
 		g.setColor(Color.black);
 		g.fillRect(0, 0, 1000, 1000);
 
 		g.setColor(Color.WHITE);
 
-//		concentricCircles(g, 500, 100, 20);
+//		concentricCircles(g, 500, screenL / 2 - 250, screenL / 2 - 250, 10);
 
 //		g.setColor(Color.ORANGE);
-//		squares(g, 350, 350, 300);
+//		squares(g, screenL / 2 - 150, screenL / 2 - 150, 300);
 
-		clover(g, 500, 500, 500);
+//		clover(g, 500, 500, 500);
 
+//		flake(g, screenL / 2, screenL / 2, 400, 0);
+
+		g.setColor(hsv2C(branchHue, 100, 100));
+		g.drawLine(screenL / 2, screenL - 50, screenL / 2, screenL - 350);
+		treeBranch(g, screenL / 2, screenL - 350, 150, pi / 2, branchHue + 30);
+
+		branchHue++;
 	}
 
 	public static void main(String[] arg) {
@@ -115,6 +189,8 @@ public class Recursion extends JPanel implements ActionListener {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(1000, 1000);
 		f.add(this);
+		t.start();
+
 		f.setVisible(true);
 
 	}
